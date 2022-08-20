c688dcf40a2f6bb84e00f8b43e917b89cb244299
OMiD <o.rasouli92@gmail.com>
2022-08-20 16:45:15 +0430

    Update event handler

--- src/scripts/eventHandler.js
+++ src/scripts/eventHandler.js
@@ -15,34 +15,25 @@ function startupEvents() {
   //Change the value of Thickness
   thicknessEl.addEventListener("change", () => {
     //Get the value and parse to int
-    thickness = parseInt(thicknessEl.value);
-    //Calculate the Width
-    canvas.style.width = `${width * thickness + width - 1}px`;
-    //Calculate the Height
-    canvas.style.height = `${height * thickness + height - 1}px`;
-    //Calculate the Background size
-    canvas.style.backgroundSize = `${thickness + 1}px ${thickness + 1}px`;
+    board.Thickness = parseInt(thicknessEl.value);
 
-    //Change the size of cells in the canvas
-    for (const cell of canvas.childNodes) {
-      cell.style.width = `${thickness}px`;
-      cell.style.height = `${thickness}px`;
-    }
+    //Update styles for cell and board
+    board.UpdateStyle();
+
     //Export them, then show the sample and replace the code
-    exportPixelArt();
+    exportAndShow();
   });
 
   //Change the value of Roundness
   roundnessEl.addEventListener("change", () => {
     //Get the value and parse to int
-    roundness = parseInt(roundnessEl.value);
+    board.Roundness = parseInt(roundnessEl.value);
 
-    //Change the border radius of cells in the canvas
-    for (const cell of canvas.childNodes) {
-      cell.style.borderRadius = `${roundness % 101}%`;
-    }
+    //Update styles for cell and board
+    board.UpdateStyle();
+
     //Export them, then show the sample and replace the code
-    exportPixelArt();
+    exportAndShow();
   });
 
   //Click on the create board button
@@ -50,16 +41,15 @@ function startupEvents() {
     //Remove data
     cleanData();
 
-    //Calculate the columns in the grid, Width and Height
-    //The Opacity is for the first time shows grid lines
-    const canvasStyle = `grid-template-columns:repeat(${width},auto);height:${
-      height * thickness + height - 1
-    }px;width:${width * thickness + width - 1}px;opacity:1;`;
-
-    //Add the style above
-    canvas.style.cssText = canvasStyle;
+    board.DefineSize(
+      +widthEl.value,
+      +heightEl.value,
+      +thicknessEl.value,
+      +roundnessEl.value,
+      canvas
+    );
 
     //Create cells
-    createCells();
+    board.BoardCreator();
   });
 }

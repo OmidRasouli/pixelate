c5a410541a9ac92bc9babdb1b15de75df91cd53a
OMiD <o.rasouli92@gmail.com>
2022-09-01 11:47:15 +0430

    Add History class

---
+++ src/scripts/history.js
@@ -0,0 +1,43 @@
+class History {
+  #historyStack;
+  #tempHistoryStack;
+
+  constructor() {
+    this.#historyStack = [];
+    this.#tempHistoryStack = [];
+  }
+
+  Undo() {
+    if (this.#historyStack.length === 0) return;
+
+    const lastAction = this.#historyStack.pop();
+    this.#tempHistoryStack.push(lastAction);
+    return lastAction.FromJson();
+  }
+
+  Redo() {
+    if (this.#tempHistoryStack.length === 0) return;
+
+    const lastAction = this.#tempHistoryStack.pop();
+    this.#historyStack.push(lastAction);
+    return lastAction.FromJson;
+  }
+
+  SaveHistory(action) {
+    const test = action.ToJSON();
+    if (
+      this.#historyStack.length > 0 &&
+      test === this.#historyStack[this.#historyStack.length - 1]
+    )
+      return;
+
+    this.#historyStack.push(test);
+    this.#tempHistoryStack = [];
+    console.log(this.#historyStack);
+  }
+
+  Clear() {
+    this.#historyStack = [];
+    this.#tempHistoryStack = [];
+  }
+}

012685450f495d6068892d18469c8d5f1f97d92b
OMiD <o.rasouli92@gmail.com>
2022-09-01 11:46:30 +0430

    Edit Exporter

--- src/scripts/exporter.js
+++ src/scripts/exporter.js
@@ -6,6 +6,12 @@ function shadowCalculation() {
   let data = [];
 
   for (const shadow of Object.keys(board.cells)) {
+    if (
+      typeof board.cells[shadow] == "function" ||
+      board.cells[shadow].BackgroundColor === "transparent"
+    )
+      continue;
+
     data.push({
       x: board.cells[shadow].Col * board.Thickness + board.Thickness,
       y: board.cells[shadow].Row * board.Thickness + board.Thickness,
@@ -27,9 +33,13 @@ function shadowCalculation() {
 function exportPixelArt() {
   const data = shadowCalculation();
 
+  const shadows = data.reduce((prev, current) => prev + current.join(), "");
+
   const boxShadow =
-    "box-shadow:" + data.reduce((prev, current) => prev + current.join(), "");
-  let style = `width: ${board.Thickness}px;\nheight: ${board.Thickness}px;\n${boxShadow};`;
+    shadows.length === 0
+      ? ""
+      : `box-shadow: ${shadows.substring(0, shadows.length - 2)};`;
+  let style = `width: ${board.Thickness}px;\nheight: ${board.Thickness}px;\n${boxShadow}`;
   if (board.Roundness > 0) {
     style += `\nborder-radius: ${board.Roundness}%;`;
   }
@@ -42,9 +52,13 @@ function showSample(data) {
   const ratio = findRatio(board.Width, board.Height, board.Thickness);
   const scaledPixel = Math.max(board.Thickness * ratio);
 
-  const boxShadow =
-    "box-shadow:" +
-    data.reduce((prev, current) => prev + current.scaleAndJoin(ratio), "");
+  const shadows = data.reduce(
+    (prev, current) => prev + current.scaleAndJoin(ratio),
+    ""
+  );
+
+  const boxShadow = shadows.length === 0 ? "" : `box-shadow: ${shadows}`;
+
   let style = `width: ${scaledPixel}px;height: ${scaledPixel}px;border-radius: ${
     board.Roundness % 101
   }%;${boxShadow};`;

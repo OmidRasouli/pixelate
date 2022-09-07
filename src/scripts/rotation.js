7aefbcacf200bbe7f9882d4442c195450370e5be
OMiD <o.rasouli92@gmail.com>
2022-09-07 09:54:19 +0430

    Change Icons for rotate

---
+++ src/scripts/rotation.js
@@ -0,0 +1,34 @@
+class Rotation {
+  rotateMode = {
+    Right90: "+90",
+    Right180: "+180",
+    Left90: "-90",
+    Left180: "-180",
+    MirrorVertical: "vertical",
+    MirrorHorizontal: "horizontal",
+  };
+
+  constructor() {}
+
+  Rotate(mode) {
+    switch (mode) {
+      case value:
+      case Right90:
+        break;
+      case Right180:
+        break;
+      case Left90:
+        break;
+      case Left180:
+        break;
+      case MirrorVertical:
+        break;
+      case MirrorHorizontal:
+        break;
+      default:
+        break;
+    }
+  }
+
+  
+}

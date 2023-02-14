84ad09ab1014a3f256c7ea3a29d24f833c41e5be
OMiD <o.rasouli92@gmail.com>
2023-02-14 19:51:10 +0330

    Add downloadable file for export

--- src/scripts/exporter.js
+++ src/scripts/exporter.js
@@ -35,6 +35,21 @@ function shadowCalculation(extraSpace) {
 function exportSVG() {
   const data = shadowCalculation(false);
   svgCreator({ ...board.Size, "borderRadius": board.Roundness }, data);
+
+  download("image/svg+xml", "svg", svg.outerHTML);
+}
+
+function download(MIME, type, data) {
+  var element = document.createElement('a');
+  element.setAttribute('href', `data:${MIME};base64,${btoa(data)}`);
+  element.setAttribute('download', `pixelate.${type}`);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
 
 function exportPixelArt() {
